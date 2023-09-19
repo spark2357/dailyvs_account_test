9919aa682c86dfa1198e77d376dba93c844fcfe8
spark2357 <psbted@naver.com>
2023-09-20 01:34:14 +0900

    asdf

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include ,re_path
 from django.views.generic import TemplateView
+from .views import page
 
 app_name = "accounts"
 
 urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('goodmansangbeen', page),
 ]
 
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
